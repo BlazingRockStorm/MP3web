6e8d6275c1caaa5c380f2a3162ba2d33e64929da
BlazingRockStorm <rocker.gryphon@gmail.com>
2017-06-20 14:12:47 +0700

    add genre show view

---
+++ resources/views/genre/show.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <h1>{{$genre->genre}}</h1>
+        @foreach ($artist as $artists)
+        <div class="row" style="margin-bottom: 10px">
+            <div class="col-md-4"><img src="{{asset('pic/'.$artists->pic.'.jpg')}}" width="370"></div>
+            <div class="col-md-6">
+
+                <h1>{{ $artists->name }}</h1>
+                <h3>{{ $artists->info }}</h3>
+                <a href="{{asset('artist/'.$artists->id)}}">Listen</a>
+            </div>
+        </div>
+        @endforeach
+</div>
+@endsection
